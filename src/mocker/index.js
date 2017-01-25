518d4ae39b532c775aa7cae633c0ef2e51364dc1
Paul <202328@sh.lianjia.com>
2017-01-25 16:40:07 +0800

    修改了接口

--- src/mocker/index.js
+++ src/mocker/index.js
@@ -6,14 +6,19 @@ import * as npmPackage from '../../package.json';
 import mockerConfig from './config';
 
 import {createMockerDatabase, addMockerRecord} from './Data';
-import {MockerHttpRequest, resetXMLHttpRequest} from './MockerHttpRequest';
+import {
+    MockerHttpRequest,
+    resetXMLHttpRequest,
+} from './MockerHttpRequest';
 
 // 初始化Mocker
 const mocker = {
     close: resetXMLHttpRequest,
     config: mockerConfig,
     version: npmPackage.version,
-    mock: addMockerRecord
+    mock: function () {
+        addMockerRecord.apply(this, arguments);
+    }
 };
 
 // 修改原生的XMLHttpRequest
